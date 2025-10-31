5afbb39034a6fe677417b240658807967cdef94f
MasameEh <samehms116@gmail.com>
2025-10-31 16:23:49 +0200

    remove unnecessary dependencies

--- wheel-datepicker-lib/build.gradle.kts
+++ wheel-datepicker-lib/build.gradle.kts
@@ -35,7 +35,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.material3)
